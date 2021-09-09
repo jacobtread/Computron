a0db2629ae96389a457580ea09445c2f6da50d93
jacobtread <jacobtread@users.noreply.github.com>
2021-09-09 07:37:29 +0000

    deploy: 0192de77f15dfc35b32e4374c6e67ad0af4b3533

---
+++ _nuxt/static/1631173048/support/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FComputron\u002F_nuxt\u002Fstatic\u002F1631173048",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsupport",config:{_app:{basePath:"\u002FComputron\u002F",assetsPath:"\u002FComputron\u002F_nuxt\u002F",cdnURL:a}}}}(null));
