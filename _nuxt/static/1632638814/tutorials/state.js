17dec23eb6c1341d33ed0c57c8cafa1bee516303
jacobtread <jacobtread@users.noreply.github.com>
2021-09-26 06:56:05 +0000

    deploy: 5066cf93195f7a3716885a5858d025bdf38fb692

--- _nuxt/static/1632638814/tutorials/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FComputron\u002F_nuxt\u002Fstatic\u002F1632638814",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials",config:{_app:{basePath:"\u002FComputron\u002F",assetsPath:"\u002FComputron\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3d2c83f0"}}}}(null));
