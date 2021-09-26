1d873e6fa7e4c127c7d3624deba3f0371bce6492
Jacobtread <jacobtread@gmail.com>
2021-09-26 19:39:41 +1300

    TextBlock improvements & Line height
    Line height on p element set globally to 1.5
    
    Renamed class .block--icon to .block--iconified for more readability 
    
    Removed numbered property from text block because it is no longer used (removed corresponding css)
    
    Moved text block centered functionality into a mixin titled "centered" so that css is not being repeated
    
    Documented all lines of css

--- components/TextBlock.vue
+++ components/TextBlock.vue
@@ -4,7 +4,7 @@
       block: true,
       'block--numbered': numbered, // Apply the block--numbered class if numbered is ture
       'block--centered': centered, // Apply the block--centered class if centered is ture
-      'block--icon': icon, // Apply the block--icon class if icon was provided
+      'block--iconified': icon, // Apply the block--iconified class if icon was provided
       'block--reversed': reversed, // Apply the block--reversed class if reversed is ture
     }"
   >
@@ -31,11 +31,12 @@
 </template>
 
 <script>
+// Export the component with its name & properties
 export default {
   name: "TextBlock",
+  // Properties this component accepts
   props: {
     title: String,
-    numbered: Boolean,
     centered: Boolean,
     icon: String,
     reversed: Boolean,
@@ -47,136 +48,134 @@ export default {
 <style scoped lang="scss">
 @import "~/assets/css/variables";
 
-.block {
-  display: block;
-  width: 100%;
-  padding: 0 1em;
-  text-align: left;
+// Defines styles for a centered text block
+@mixin centered {
+  text-align: center; // Align the text to center
+  /* 
+    Flow the content in a reversed column this will
+    place the image at the top and the text at the bottom
+    in a nice neat column
+  */
+  flex-flow: column-reverse;
 
-  h1 {
-    display: inline-block;
-    position: relative;
-    color: black;
-    margin-bottom: 0.9em;
-
-    &::before {
-      content: "";
-      position: absolute;
-      left: 0;
-      top: 120%;
-      width: 80%;
-      height: 4px;
-      background-color: $primary;
-    }
+  // Target the pseudo element before the title (h1)
+  h1::before {
+    left: 50%; // Place element half way accross parent
+    transform: translateX(
+      -50%
+    ); // Move the element half its width back (aligns to center)
   }
 
-  &--icon {
-    display: flex;
-    flex-flow: row;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  &--reversed {
-    flex-flow: row-reverse;
-    text-align: right;
-
-    h1::before {
-      left: auto;
-      right: 0;
-    }
-    .buttons {
-      justify-content: flex-end;
-    }
-  }
-  &__text {
-    flex: auto;
-    max-width: 500px;
-  }
-
-  img {
-    max-height: 200px;
-    width: auto;
-    height: auto;
-  }
-
-  &--numbered {
-    h1 {
-      font-size: 4em;
-
-      &::before {
-        width: 150%;
-      }
-    }
-  }
-
+  // The text content
   p {
-    line-height: 1.5;
+    margin: 0 auto; // Align the fixed with to center with margins
+    max-width: 600px; // Cap the text block width
+  }
+
+  // The buttons wrapper
+  .buttons {
+    justify-content: center; // Place the content in the center
+  }
+}
+
+// The text block
+.block {
+  display: block; // Display the content as a block
+  width: 100%; // Take up the full width
+  padding: 0 1em; // Apply no vertical padding but 1em of horizontal padding
+  text-align: left; // Align the text to the left
+
+  // The title element of the text block
+  h1 {
+    display: inline-block; // Display as an inline block to keep its width while acting like a block
+    position: relative; // This element needs to be relative positioning to be the parent of the underline
+    color: black; // Set the text color to black
+    margin-bottom: 0.9em; // Add a margin to the bottom 0.9x the font size
+
+    // Target the pseudo element before the title (h1)
+    &::before {
+      /* 
+        This pseudo element is used to create the 
+        underline effect that is under the title
+      */
+      content: ""; // Give it an empty content
+      position: absolute; // Set its position to absolute
+      left: 0; // Put it all the way to the left of the parent
+      top: 120%; // Put it at the bottom +20% of the parent
+      width: 80%; // Make it 80% of the parent width
+      height: 4px; // Make the line only 4px
+      background-color: $primary; // Set its fill color to the primary (blue) colour
+    }
+  }
+
+  // (.block__text) The text portion of the block if its an icon block
+  &__text {
+    flex: auto; // Automatically flex this box
+    max-width: 500px; // Max the width of this box to 500px
+  }
+
+  // The icon image
+  img {
+    max-height: 200px; // Max the height of the image to 200px
+    width: auto; // Automatically set the width
+    height: auto; // Automatically set the height
+  }
+
+  // (.block--iconified) Applied if this block has an icon
+  &--iconified {
+    display: flex; // Display the content as a flexbox
+    flex-flow: row; // Flex the content horizontally
+    align-items: center; // Vertically align the content to center
+    justify-content: space-between; // Space between the icons and text block
+  }
+
+  // (.block--reversed) Applied if this block should be reversed
+  &--reversed {
+    flex-flow: row-reverse; // The same as row but goes in reverse
+    text-align: right; // Align the text to the right
+
+    // Target the pseudo element before the title (h1)
+    h1::before {
+      left: auto; // Align the left automatically
+      right: 0; // Place at the right of the element
+    }
+
+    // The buttons wrapper
+    .buttons {
+      justify-content: flex-end; // Place the content at the end of the flexbox (the right)
+    }
   }
 
   &--centered {
-    text-align: center;
-
-    h1::before {
-      left: 50%;
-      right: auto;
-      transform: translateX(-50%);
-    }
-
-    p {
-      margin: 0 auto;
-      max-width: 600px;
-    }
-
-    .buttons {
-      justify-content: center;
-    }
-
-    .extra {
-      text-align: center;
-    }
+    // Include the styles for a centered block
+    @include centered();
   }
 }
 
+// The button wrapper
 .buttons {
-  display: flex;
-  flex-flow: row;
-  flex-wrap: wrap;
-  margin-top: 1em;
-
-  .button {
-    display: inline-block;
-  }
+  display: flex; // Display the buttons as a flexbox
+  flex-flow: row; // Flex the items in a row
+  flex-wrap: wrap; // Allow the items to wrap
+  margin-top: 1em; // Add a margin of 1x the font size to the top
 }
 
-.extra {
-  display: block;
-  text-align: left;
-
-  ::v-deep a {
-    display: block;
-    color: #343534;
-    margin-top: 1.5em;
-  }
+// The extra content link elements
+.extra a {
+  display: block; // Display the text as its own block
+  color: $extra-link-colour; // Set the text colour
+  margin-top: 1.5em; // Apply a margin to the top
 }
 
 @media screen and (max-width: 700px) {
+  /* 
+    On smaller displays (less than 700px wide) all text
+    blocks are converted to centered text blocks
+  */
+  // The block element
   .block {
-    flex-flow: column-reverse;
-    text-align: center;
-
-    &--reversed .buttons {
-      justify-content: center;
-    }
-    h1::before {
-      left: 50%;
-      right: auto;
-      transform: translateX(-50%);
-    }
-  }
-
-  .buttons {
-    justify-content: center;
+    // Include the styles for a centered block
+    @include centered();
   }
 }
 </style>
