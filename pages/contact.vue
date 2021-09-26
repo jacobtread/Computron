5fc451fc4764ad60ab775220eb2cfa7a5463e2f1
Jacobtread <jacobtread@gmail.com>
2021-09-26 20:20:17 +1300

    Removed unused template elements

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,13 +2,11 @@
   <div class="blocks">
     <!-- The welcome block -->
     <TextBlock title="Get in contact" centered>
-      <template>
-        <!-- The text content for the block -->
-        You can get in touch with us via email to arrange a meeting to either
-        learn more about Computron Or you can just email us about any concerns,
-        interests or issues you have with Computron we will get back to you as
-        soon as possible.
-      </template>
+      <!-- The text content for the block -->
+      You can get in touch with us via email to arrange a meeting to either
+      learn more about Computron Or you can just email us about any concerns,
+      interests or issues you have with Computron we will get back to you as
+      soon as possible.
       <template #buttons>
         <!-- The buttons block for additional buttons -->
         <a class="button" href="mailto:jsoh@computron.co.nz">Email</a>
@@ -24,13 +22,11 @@
       icon="support.svg"
       icon-name="Support Icon"
     >
-      <template>
-        <!-- The text content for the block -->
-        If you need help with your devices we may be able to help your remotely.
-        We have an easy to use tutorial to help you get setup before we help you
-        remotely this tutorial is intended to be followed before contacting us
-        in order to help speed up the process of us helping you
-      </template>
+      <!-- The text content for the block -->
+      If you need help with your devices we may be able to help your remotely.
+      We have an easy to use tutorial to help you get setup before we help you
+      remotely this tutorial is intended to be followed before contacting us in
+      order to help speed up the process of us helping you
       <template #buttons>
         <!-- The buttons block for additional buttons -->
         <NuxtLink to="/tutorials/support-setup" class="button">
