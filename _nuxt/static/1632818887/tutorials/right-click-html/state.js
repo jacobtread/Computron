211480d4b2dc45b574d83ef92233318679508acd
jacobtread <jacobtread@users.noreply.github.com>
2021-09-28 08:50:57 +0000

    deploy: ec46ff41fc37d3cb2c41ec11722a7a89ab7f61a4

--- _nuxt/static/1632818887/tutorials/right-click-html/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FComputron\u002F_nuxt\u002Fstatic\u002F1632818887",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fright-click-html",config:{_app:{basePath:"\u002FComputron\u002F",assetsPath:"\u002FComputron\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"62d092bc"}}}}(null));
