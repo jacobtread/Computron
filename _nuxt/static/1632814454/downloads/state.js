2420e025fa8c200402d2289badfe1818bc974b15
jacobtread <jacobtread@users.noreply.github.com>
2021-09-28 08:09:44 +0000

    deploy: 8b91091f7fbfefdb431314c294e0abe42791dd37

--- _nuxt/static/1632814454/downloads/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FComputron\u002F_nuxt\u002Fstatic\u002F1632814454",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdownloads",config:{_app:{basePath:"\u002FComputron\u002F",assetsPath:"\u002FComputron\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"136d3257"}}}}(null));
