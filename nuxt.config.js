d8b9e1203ec11476ad150a2578c6d8094a6ced68
Jacobtread <jacobtread@gmail.com>
2021-10-16 06:14:34 +1300

    GitHub pages env change

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,38 +10,38 @@ export default {
     // Provide the page meta tags
     meta: [
       // Set the encoding to utf-8
-      {charset: 'utf-8'},
+      { charset: 'utf-8' },
       // Set the window viewport properties
 
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       // Website Meta (author, keywords, etc)
-      {name: 'author', content: 'Jacob Read'},
+      { name: 'author', content: 'Jacob Read' },
       {
         hid: 'description',
         name: 'description',
         content: 'Computron offers computer/networking repairs, Helpful download links to everyday software, and provides training for computers & applications'
       },
-      {name: 'keywords', content: 'computers,networking,repairs,software,downloads,training'},
-      {name: 'format-detection', content: 'telephone=no'},
+      { name: 'keywords', content: 'computers,networking,repairs,software,downloads,training' },
+      { name: 'format-detection', content: 'telephone=no' },
       // Open Graph Protocol Meta (see https://opengraphprotocol.org/)
-      {name: 'og:title', content: 'Computron'},
-      {name: 'og:type', content: 'website'},
+      { name: 'og:title', content: 'Computron' },
+      { name: 'og:type', content: 'website' },
       {
         name: 'og:description',
         content: 'Computron offers computer/networking repairs, Helpful download links to everyday software, and provides training for computers & applications'
       }
     ],
     link: [
-      {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
-      {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: true},
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'}
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: true },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' }
     ]
   },
   components: true,
   router: {
     // Make sure the router adds /Computron/ to the url start (This is for github pages)
-    base: '/Computron/'
+    base: process.env.GITHUB_PAGES ? '/Computron/' : '/'
   },
   hooks: {
     // This hook is run before all the files are parsed by nuxt
