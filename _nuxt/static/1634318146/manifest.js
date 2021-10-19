be1a9b34eaeab8d0eca5bc0033a8a4a58182b7dd
jacobtread <jacobtread@users.noreply.github.com>
2021-10-19 02:49:10 +0000

    deploy: 26eccd308865a1865172585a6b4aff6c2fb156d4

--- _nuxt/static/1634318146/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact","\u002F","\u002Fdownloads","\u002Ftutorials","\u002Ftutorials\u002Fsupport-setup","\u002Ftutorials\u002Fright-click-html","\u002Ftutorials\u002Futorrent"]})
